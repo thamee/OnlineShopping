35ed1ae1b447abaab5f460ba328d12ecf8976fa9
Thamera <Thamera@speedcodelabs.com>
2021-06-28 06:40:35 +0530

    Add Login Screen Validations and Error Messages

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,22 +15,43 @@ export class LoginComponent implements OnInit {
     email : "",
     password:""
   }
+  errors!:[];
   error = {
     name: '',
-    password: null
+    password: ''
   };  
   
   ngOnInit(): void {
   }
 
   Login(){
+    if (this.validate()) {
+   
     this.authService.login(this.form.email,this.form.password).subscribe((authResponse: AuthResponce) => {
-
       localStorage.setItem('token', JSON.stringify(authResponse.token));
       this.router.navigateByUrl(`/`);
       
+    },error=>{
+      if(error && error.error&& error.error.errors)      {
+        this.errors=error.error.errors
+      }
     })
+  }
 
+  }
+  validate(): boolean {
+    this.error.name = '';this.error.password = '';
+    if (!this.form.email || this.form.email.length <=0) {
+      this.error.name = "Usename is required";
+      return false;
+    }
+     else if (!this.form.password || this.form.password.length <=0) {
+        this.error.password = "Password is required";
+      return false;
+    } else { this.error.name = '';this.error.password = '' }
+  
+   
+    return true;
   }
 
 }
