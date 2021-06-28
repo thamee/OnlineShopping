27d4a1dd59cbbcd9d4a83aa080e015bc0cfc8d8b
Thamera <Thamera@speedcodelabs.com>
2021-06-28 06:58:17 +0530

    Added Register Validations

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -18,18 +18,39 @@ export class RegisterComponent implements OnInit {
   }
   error = {
     name: '',
-    password: null
+    password: ''
   };  
+  errors!:[];
 
 
   ngOnInit(): void {
   }
 
   Register(){
+    if (this.validate()) {
     this.authService.register(this.form.email,this.form.password).subscribe((authResponse: AuthResponce) => {
       localStorage.setItem('token', JSON.stringify(authResponse.token));
       this.router.navigateByUrl(`/`);
       
+    },error=>{
+      if(error && error.error&& error.error.errors)      {
+        this.errors=error.error.errors
+      }
     })
+  }
+  }
+  validate(): boolean {
+    this.error.name = '';this.error.password = '';
+    if (!this.form.email || this.form.email.length <=0) {
+      this.error.name = "Usename is required";
+      return false;
+    }
+     else if (!this.form.password || this.form.password.length <=0) {
+        this.error.password = "Password is required";
+      return false;
+    } else { this.error.name = '';this.error.password = '' }
+  
+   
+    return true;
   }
 }
